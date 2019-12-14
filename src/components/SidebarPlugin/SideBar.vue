f7b3b2bdfd8b55e9b516a9505cd372b61bba2a93
Alan Yin <alan110664@gmail.com>
2019-12-14 20:34:04 +0800

    new UI

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -43,7 +43,7 @@ export default {
   props: {
     title: {
       type: String,
-      default: "Paper Dashboard"
+      default: "Smart Housing"
     },
     backgroundColor: {
       type: String,
